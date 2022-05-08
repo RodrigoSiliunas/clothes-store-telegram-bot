8a82078bb1ad46580d7757e706d97231959806ce
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2022-05-07 23:34:27 -0300

    make it better

--- src/routes/orders/orders.py
+++ src/routes/orders/orders.py
@@ -10,29 +10,25 @@ def main_page(update: Update, callback: CallbackContext) -> int:
     query = update.callback_query
     query.answer()
 
+    user = SOLDED_COLLECTION.find_one(
+        { 'identifier': update.effective_user.id }
+    )
+    orders = user['types']
+    page = user['page']
+
+    # If user don't have any registred orders in database, return a message and do nothing.
+    if (len(orders) < 1):
+        callback.bot.send_message(
+            'Você não possui nenhum item em seus últimos pedidos.')
+        return
+
     reply_markup = get_reply_markup()
 
     message_to_send = f'Mano, não tô entendendo mais nada!!!'
     query.edit_message_text(
         text=message_to_send, reply_markup=reply_markup, parse_mode='Markdown')
 
-    # query = update.callback_query
-    # query.answer()
 
-    # print('isso está sendo clicado')
-
-    # user = SOLDED_COLLECTION.find_one(
-    #     {'identifier': update.effective_user.id}
-    # )
-    # orders = user['types']
-    # page = user['page']
-
-    # # # If user don't have any registred orders in database, return a message and do nothing.
-    # # if (len(orders) < 1):
-    # #     print("travou nessa buceta")
-    # #     callback.bot.send_message(
-    # #         'Você não possui nenhum item em seus últimos pedidos.')
-    # #     return
 
     # message_to_send = get_default_message(
     #     identifier=orders[page]['identifier'],
